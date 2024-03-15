ea84128a07959bbba8e73e22f615d277ed0b6001
Atelier Angel Karagiozov <angelkararagiozov@gmail.com>
2024-03-15 17:50:16 +0200

    15/03/24

---
+++ app/(www)/components/Motion/PlayInView.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { ReactNode } from "react";
+
+interface PlayInViewProps {
+  children: ReactNode;
+}
+
+const PlayInView = ({ children }: PlayInViewProps) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Change this to false if you want the animation to trigger again when it comes back in view
+  });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{
+        opacity: 0.7,
+        y: 0,
+        // scale: 0.9, // Initial scale for elastic effect
+      }}
+      animate={{
+        opacity: inView ? 1 : 0.7,
+        y: inView ? 0 : 10,
+        // scale: inView ? 1 : 0.9, // Return to normal scale
+      }}
+      transition={{
+        delay: 0.5,
+        duration: 0.5,
+        ease: "linear",
+        // type: "spring", // Apply spring physics
+        // stiffness: 300, // Adjust stiffness as needed
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default PlayInView;
--- app/(www)/components/Projects/ListComponent.tsx
+++ app/(www)/components/Projects/ListComponent.tsx
@@ -4,53 +4,56 @@ import { urlForImage } from "@/sanity/lib/image";
 import Link from "next/link";
 import { Project } from "@/sanity/lib/utils";
 import { MotionDiv } from "../Motion/MotionDiv";
+import PlayInView from "../Motion/PlayInView";
 
 const ListComponent = async ({ project }: { project: Project }) => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
   return (
     <div>
-      <MotionDiv
-        initial={{
-          opacity: 0.3,
-          scale: 0.9, // Initial scale for elastic effect
-        }}
-        animate={{
-          opacity: 1,
-          scale: 1, // Return to normal scale
-        }}
-        transition={{
-          delay: project.index * 0.2,
-          duration: 0.2,
-          ease: "linear",
-          type: "spring", // Apply spring physics
-          stiffness: 300, // Adjust stiffness as needed
-        }}
-      >
-        <div className="p-2 pt-0.5 border border-dotted border-neutral  hover:text-dark dark:hover:text-gray hover:border-solid hover:bg-white bg-light dark:bg-black dark:hover:bg-blacks h-[88px] transition-all ease-in-out duration-1000">
-          <Link href={`/projects/${project?.slug?.current}`}>
-            <div className="flex justify-between">
-              <h2 className="text-sm text-blue dark:text-yellow">
-                {project?.title}
-              </h2>
+      <PlayInView>
+        <MotionDiv
+          initial={{
+            opacity: 0.3,
+            scale: 0.9, // Initial scale for elastic effect
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1, // Return to normal scale
+          }}
+          transition={{
+            delay: project.index * 0.2,
+            duration: 0.5,
+            ease: "linear",
+            type: "spring", // Apply spring physics
+            stiffness: 300, // Adjust stiffness as needed
+          }}
+        >
+          <div className="p-2 pt-0.5 border border-dotted border-neutral  hover:text-dark dark:hover:text-gray hover:border-solid hover:bg-white bg-light dark:bg-black dark:hover:bg-blacks h-[88px] transition-all ease-in-out duration-1000">
+            <Link href={`/projects/${project?.slug?.current}`}>
+              <div className="flex justify-between">
+                <h2 className="text-sm text-blue dark:text-yellow">
+                  {project?.title}
+                </h2>
 
-              <p className={`text-2xs text-gray dark:text-dark col-span-2`}>
-                {new Date(project?.publishedAt).toDateString()}
-              </p>
-            </div>
-            <div className="grid grid-rows-3 grid-flow-col gap-4">
-              <div className="grayscale w-28 hover:grayscale-0 row-span-3">
-                <Image
-                  src={urlForImage(project.cover).url()}
-                  width={100}
-                  height={70}
-                  alt="projects"
-                />
+                <p className={`text-2xs text-gray dark:text-dark col-span-2`}>
+                  {new Date(project?.publishedAt).toDateString()}
+                </p>
               </div>
-            </div>
-          </Link>
-        </div>
-      </MotionDiv>
+              <div className="grid grid-rows-3 grid-flow-col gap-4">
+                <div className="grayscale w-28 hover:grayscale-0 row-span-3">
+                  <Image
+                    src={urlForImage(project.cover).url()}
+                    width={100}
+                    height={70}
+                    alt="projects"
+                  />
+                </div>
+              </div>
+            </Link>
+          </div>
+        </MotionDiv>
+      </PlayInView>
     </div>
   );
 };
